fe9e5081b0d09d2f8b35ad73dfb5d95de3cf9154
mingwencode <mingwen314@gmail.com>
2024-07-16 21:39:15 -0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import {
+  hasDigits,
+  hasLower,
+  hasUpper,
+  hasSpecial
+} from './util.js'
 
-function App() {
+export default function App() {
+  const [passWord, setPassWord] = useState('')
+  const [score, setScore] = useState(0)
+  const [strength, setStrength] = useState('')
+
+
+  const handleOnChange =(e) => {
+    setPassWord(e.target.value)
+  }
+
+  useEffect(()=>{
+    let totalScore = 0;
+    let scoreByChar = 0;
+    let scoreByLen = 0;
+    let strengthText = ''
+    if(passWord.length > 3) {
+      scoreByLen = Math.min(6, Math.floor(passWord.length/3))
+      if(hasDigits.test(passWord)) scoreByChar += 1;
+      if(hasLower.test(passWord)) scoreByChar += 1;
+      if(hasUpper.test(passWord)) scoreByChar += 1;
+      if(hasSpecial.test(passWord)) scoreByChar += 1;
+      totalScore = scoreByChar+scoreByLen;
+
+    } else {
+      totalScore = 0;
+    }
+
+    if(totalScore > 3 && totalScore <=6) {
+      strengthText = ' / weak'
+    } else if (totalScore > 6 && totalScore <= 8) {
+      strengthText = ' / moderate'
+    } else if (totalScore > 8) {
+      strengthText = ' / strong'
+    }
+
+    setScore(totalScore)
+    setStrength(strengthText)
+
+}, [passWord])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h2>Password Strength Checker</h2>
+      <input style={ {width:"70%"}} type="text" maxLength="32" value={passWord} onChange = {handleOnChange}/>
+      <progress style={{ width: "70%" }} max="10" value={score}>{score}</progress>
+      <p>password strength is (out of 10): {score}{strength}</p>
+
+
+    </main>
   );
 }
-
-export default App;
---
+++ src/util.js
@@ -0,0 +1,7 @@
+export const hasDigits = /\d/;
+
+export const hasLower = /[a-z]/;
+
+export const hasUpper = /[A-Z]/;
+
+export const hasSpecial = /[!@#$%^&*(),.?":{}|<>]/;
